d0af12193c02c9f6fad440f412444e3b4dc3499c
liangfangyi <liangf1@airproducts.com>
2014-06-22 15:50:46 +0800

    edit the file, add more comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,6 +1,11 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
+## The first function will create a special matrix which actually is a list contain
+## several functions.
+## The second one will calculate the inverse matrix of the one created by the first
+## one, but before that, it will check if there is already the answer cached.
+
 ## Write a short comment describing this function
 ## This function generate a special matrix
 makeCacheMatrix <- function(x = matrix()) {
